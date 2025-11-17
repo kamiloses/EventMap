ad65fbbb568a0f766dca7657b036a88f6c994fa0
kamiloses <kamil.kurzaj04@gmail.com>
2025-11-17 14:03:34 +0100

    Added Event-component

---
+++ src/app/home-layout/home-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [],
+  templateUrl: './home-layout.component.html',
+  styleUrl: './home-layout.component.css',
+})
+export class HomeLayoutComponent {
+
+}
